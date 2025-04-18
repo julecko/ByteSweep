ae4f42a0bc065298d69873bb97ba3683da7d22dc
Julecko <julecko@dilino.sk>
2025-04-19 01:27:30 +0200

    Started working on design, mostly sidebar for now

---
+++ Components/Header.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ByteSweep.Components
+{
+    public partial class Header : UserControl
+    {
+        public Header()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Components/Sidebar.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ByteSweep.Components
+{
+    public partial class Sidebar : UserControl
+    {
+        public Sidebar()
+        {
+            InitializeComponent();
+        }
+
+        private void SidebarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = (sender as Button)?.Tag?.ToString();
+            MessageBox.Show($"Clicked: {tag}");
+        }
+    }
+}
